f28f88ad0ad82d4c8b9f4cfe0b89fe7839cc9707
Gerard Geer <gdg1546@rit.edu>
2013-03-01 18:58:09 -0800

    Documented AnimationBay.h

---
+++ AnimationBay.h
@@ -0,0 +1,67 @@
+#include <vector>
+#include "Animation.h"
+class AnimationBay{
+	public:
+	
+		/*
+		Constructs the AnimationBay.
+		*/
+		AnimationBay(void);
+	
+		/*
+		Destructs the AnimationBay.
+		*/
+		~AnimationBay(void);
+	
+		/*
+		Initializes the AnimationBay.
+		*/
+		bool init(GLCamera * camera);
+		
+	
+		/*
+		Adds a new Animation to the bay. This actually constructs an animation and places it in the
+		underlying vector.
+		*/
+		bool addAnimation(GLTexture * texture, ShaderProgram * program, float frameTime, int numFrames, 
+							float x=0, float y=0, float z=0, float width=0, float height=0);
+		
+		/*
+		Returns an Animation by searching for it in the arraylist.
+		*/
+		Animation * getAnimationByID(int id);
+		
+		/*
+		Removes an Animation from the bay and returns it whole.
+		*/
+		Animation removeAnimationByID(int id);
+		
+		/*
+		Deletes an Animation without returning it.
+		*/
+		bool deleteAnimationByID(int id);
+		
+		/*
+		Sets the camera that is supplied to all of the Animations in the bay.
+		*/
+		void setCamera(GLCamera * camera);
+		
+		/*
+		Returns the camera that is supplied to all of the Animations in the bay.
+		*/
+		GLCamera * getCamera(void);
+	
+	private:
+	
+		/*
+		The camera that is common to all of the Animations in the bay.
+		*/
+		GLCamera * camera;
+		
+		/*
+		The vector that stores the Animations of this bay.
+		*/
+		std::vector<Animation> animations;
+		
+		
+};
