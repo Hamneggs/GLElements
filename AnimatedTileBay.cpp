bc0b5e9f1344dc5b4e6a0361906c739925b34164
Gerard Geer <gdg1546@rit.edu>
2013-03-12 19:31:27 -0700

    Created AnimatedTileBay

---
+++ AnimatedTileBay.cpp
@@ -0,0 +1,97 @@
+#include "AnimatedTileBay.h"
+
+AnimatedTileBay::AnimatedTileBay(void)
+{
+	currentID = 0;
+}
+
+AnimatedTileBay::~AnimatedTileBay(void)
+{
+	// Blah
+}
+
+bool AnimatedTileBay::init(void)
+{
+	return true;
+}
+
+AnimatedTileBay::addTile(Animation * animation, float x=0, float y=0, float z=0, 
+						float width=0, float height=0)
+{
+	unsigned int previousSize = tiles.size();
+	tiles.push_back(AnimatedTile(animation, x, y, z, width, height, currentID);
+	
+	unsigned int currentSize = tiles.size();
+	return (current == previousSize+1);
+}
+
+AnimatedTileBay::getTileByID(int id)
+{
+	for(unsigned int i = id; i >= 0; i--)
+	{
+		if(i >= tiles.size())
+		{
+			continue;
+		}
+		else if(tiles[i].id == id)
+		{
+			return &tiles[i];
+		}
+	}
+}
+
+AnimatedTileBay::removeTileByID(int id)
+{
+	AnimatedTile returnMe;
+	unsigned int indexOf = 99999999;
+	for(unsigned int i = id; i >= 0; i--)
+	{
+		if(i >= tiles.size())
+		{
+			continue;
+		}
+		else if(tiles[i].id == id)
+		{
+			returnMe = tiles[i];
+			indexOf = i;
+			break;
+		}
+	}
+	tiles.erase(tiles.begin()+indexOf);
+	return returnMe;
+}
+
+AnimatedTileBay::deleteTileByID(int id)
+{
+	AnimatedTile returnMe;
+	unsigned int indexOf = 99999999;
+	bool found = false;
+	for(unsigned int i = id; i >= 0; i--)
+	{
+		if(i >= tiles.size())
+		{
+			continue;
+		}
+		else if(tiles[i].id == id)
+		{
+			returnMe = tiles[i];
+			indexOf = i;
+			found = true;
+			break;
+		}
+	}
+	if(!found)
+	{
+		return false;
+	}
+	else
+	{
+		tiles.erase(tiles.begin()+indexOf);
+		return true;
+	}
+}
+
+AnimatedTileBay::getNumTiles(void)
+{
+	return tiles.size();
+}
---
+++ AnimatedTileBay.h
@@ -0,0 +1,62 @@
+#ifndef ANIMATED_TILE_BAY_GUARD
+#define ANIMATED_TILE_BAY_GUARD
+
+#include <vector>
+#include "AnimatedTile.h"
+
+/*
+	This class is meant to be a collection of Animated Tiles.
+	This allows for a sprite-table ideology, and for any Animated Tile
+	in the bay to be drawn repeatedly rather than to exist repeatedly.
+*/
+class AnimatedTileBay{
+	public:
+		AnimatedTileBay(void);
+		~AnimatedTileBay(void);
+		
+		/*
+		Initializes the Animation Bay. This function is included for solidarity
+		of form with other bays.
+		*/
+		bool init(void);
+		
+		/*
+		Constructs and adds an animated tile to the bay.
+		*/
+		bool addTile(Animation * animation, float x=0, float y=0, float z=0, 
+					float width=0, float height=0);
+					
+		/*
+		Returns an Animation by searching for it in the arraylist.
+		*/
+		Animation * getTileByID(int id);
+		
+		/*
+		Removes an Animation from the bay and returns it whole.
+		*/
+		Animation removeTileByID(int id);
+		
+		/*
+		Deletes an Animation without returning it.
+		*/
+		bool deleteTileByID(int id);
+		
+		/*
+		Returns the number of Animated Tiles in the bay.
+		*/
+		unsigned int getNumTiles(void);
+		
+		
+	private:
+	
+		/*
+		The vector that stores the Animated Tiles of this bay.
+		*/
+		std::vector<Animation> tiles;
+		
+		/*
+		The current ID.
+		*/
+		unsigned int currentID;
+		
+};
